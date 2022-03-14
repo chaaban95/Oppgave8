e504b2b0e2ab97d0cfbb21cb2c70cf52a2600b96
chaaban95 <78092566+chaaban95@users.noreply.github.com>
2022-03-14 01:05:28 +0100

    update

--- my-app/src/components/Movies.js
+++ my-app/src/components/Movies.js
@@ -5,22 +5,18 @@ export default function Movies() {
     const [ data, setData] = useState([])
 
     useEffect(()=>{
-
+        const getMoviesData = async () => {
+            const movies = await getMovies()
+            setData(movies)
+            console.log(movies)
+        }
+        getMoviesData()
     }, [])
-
-    const getMoviesData = async () => {
-        const movies = await getMovies()
-        setData(movies)
-        console.log(movies)
-    }
     
     return (
         <>
             <h2>Hei</h2>
             {data?.length > 0 ? <p>{JSON.stringify(data)}</p> : null}
-            <button type="button" onClick={getMoviesData}>
-                Hent data
-            </button>
         </>
     )
 }
